6312707bc1d209e691e9d188a912b1af81f9f146
natan-alex <natancarmo321@gmail.com>
2021-09-04 12:48:23 -0300

    Reorder the methods in graph.h to have the same order as they appear in graph.cpp

--- graph.h
+++ graph.h
@@ -59,24 +59,24 @@ namespace AboutGraphs
 
         void fill_edge_list_and_vertice_set();
         void fill_graph_properties();
+        void check_if_there_are_directed_and_undirected_edges_at_the_same_time();
+        void check_if_there_are_pondered_and_unpondered_edges_at_the_same_time();
+        void check_if_the_graph_is_directed_based_on_the_statistics();
+        void check_if_the_graph_is_pondered_based_on_the_statistics();
+
         void fill_representations();
         void fill_successor_adjacency_list();
         void fill_predecessor_adjacency_list();
+        void show_adjacency_list(list<list<string>> adjacency_list);
+
         void fill_adjacency_matrix();
+        int find_the_index_of_the_vertice(string &vertice);
         void fill_incidency_matrix();
+
+        void fill_adjacency_arrays_increasing_their_indices_by_one();
         void reorder_successor_adjacency_arrays();
         void reorder_predecessor_adjacency_arrays();
-        void fill_adjacency_arrays_increasing_their_indices_by_one();
         void order_adjacency_arrays(int which_to_sort[], int other_array[], size_t size);
-
-        void check_if_the_graph_is_directed_based_on_the_statistics();
-        void check_if_the_graph_is_pondered_based_on_the_statistics();
-        void check_if_there_are_directed_and_undirected_edges_at_the_same_time();
-        void check_if_there_are_pondered_and_unpondered_edges_at_the_same_time();
-
-        void show_adjacency_list(list<list<string>> adjacency_list);
-        int find_the_index_of_the_vertice(string &vertice);
-
         int *get_reordered_sorted_adjacency_array(int sorted_array[]);
         void show_adjacency_arrays(int sorted_array[], int other_array[]);
         void show_vertices_set();
