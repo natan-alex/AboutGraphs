294b08f845e9ccf17e1d450e0e42c66d83c3a9df
natan-alex <natancarmo321@gmail.com>
2021-11-09 08:57:21 -0300

    sorting children in bfs before add to verticesToBeExplored

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -1,17 +1,42 @@
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class BreadthFirstSearch extends BaseSearchStructure {
     private final SuccessorAdjacencyList successorAdjacencyList;
     private final Queue<Vertice> verticesToBeExplored;
     private Vertice currentVertice;
     private int verticeIndexInVerticeSet;
+    private final VerticeComparatorInBFS verticeComparatorInBFS;
+
+    private final class VerticeComparatorInBFS implements Comparator<Vertice> {
+        @Override
+        public int compare(Vertice arg0, Vertice arg1) {
+            float evaluationFunctionValueForArg0 = getEvaluationFunctionValueForVertices(currentVertice, arg0);
+            float evaluationFunctionValueForArg1 = getEvaluationFunctionValueForVertices(currentVertice, arg1);
+
+            return Float.compare(evaluationFunctionValueForArg0, evaluationFunctionValueForArg1);
+        }
+
+        private float getEvaluationFunctionValueForVertices(Vertice firstVertice, Vertice secondVertice) {
+            Edge edgeThatContainsTheVertices = getEdgeThatContainsThisVertices(firstVertice, secondVertice);
+            float evaluationFunctionValue = Float.MAX_VALUE;
+
+            if (edgeThatContainsTheVertices != null) {
+                evaluationFunctionValue = edgeThatContainsTheVertices.value;
+            }
+
+            return evaluationFunctionValue;
+        }
+    }
 
     protected BreadthFirstSearch(Graph graph, SuccessorAdjacencyList graphSuccessorAdjacencyList) {
         super(graph);
         successorAdjacencyList = graphSuccessorAdjacencyList;
 
         verticesToBeExplored = new ArrayDeque<>(relatedGraph.numberOfVertices);
+        verticeComparatorInBFS = new VerticeComparatorInBFS();
 
         performBreadthSearchAndComputeTimesInArrays();
     }
@@ -37,7 +62,10 @@ public class BreadthFirstSearch extends BaseSearchStructure {
     }
 
     private void addCurrentVerticeChildrenToNotExploredVertices() {
-        for (Vertice vertice : successorAdjacencyList.adjacencyList.get(currentVertice)) {
+        var currentVerticeChildren = successorAdjacencyList.adjacencyList.get(currentVertice);
+        Collections.sort(currentVerticeChildren, verticeComparatorInBFS);
+
+        for (Vertice vertice : currentVerticeChildren) {
             if (!verticesToBeExplored.contains(vertice) && discoveryTimes[relatedGraph.verticesAndTheirIndices.get(vertice)] == -1) {
                 verticesToBeExplored.add(vertice);
             }
