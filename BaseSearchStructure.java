01396b1338dc4335d9b5bc79a3edaaf64c68011d
natan-alex <natancarmo321@gmail.com>
2021-11-10 07:10:42 -0300

    add method to initiliaze the time arrays and rename entries in map iterators

--- BaseSearchStructure.java
+++ BaseSearchStructure.java
@@ -12,7 +12,11 @@ public abstract class BaseSearchStructure {
         endTimes = new int[graph.numberOfVertices];
         edgeClassifier = new EdgeClassifier(relatedGraph, discoveryTimes, endTimes);
 
-        for (int i = 0; i < graph.numberOfVertices; i++) {
+        initializeTimeArrays(graph.numberOfVertices);
+    }
+
+    private void initializeTimeArrays(int numberOfVertices) {
+        for (int i = 0; i < numberOfVertices; i++) {
             discoveryTimes[i] = -1;
             endTimes[i] = -1;
         }
@@ -31,9 +35,9 @@ public abstract class BaseSearchStructure {
     }
 
     protected Vertice getNextNotDiscoveredVerticeBasedOnVerticeSet() {
-        for (Map.Entry<Vertice, Integer> entry : relatedGraph.verticesAndTheirIndices.entrySet()) {
-            if (discoveryTimes[entry.getValue()] == -1) {
-                return entry.getKey();
+        for (Map.Entry<Vertice, Integer> verticeMapEntry : relatedGraph.verticesAndTheirIndices.entrySet()) {
+            if (discoveryTimes[verticeMapEntry.getValue()] == -1) {
+                return verticeMapEntry.getKey();
             }
         }
 
@@ -41,10 +45,13 @@ public abstract class BaseSearchStructure {
     }
 
     protected Edge getEdgeThatContainsThisVertices(Vertice firstVertice, Vertice secondVertice) {
-        for (Map.Entry<Edge, Integer> entry : relatedGraph.edgesAndTheirIndices.entrySet()) {
-            if (entry.getKey().firstVertice.equals(firstVertice)
-                    && entry.getKey().secondVertice.equals(secondVertice)) {
-                return entry.getKey();
+        Edge currentEdge;
+
+        for (Map.Entry<Edge, Integer> edgeMapEntry : relatedGraph.edgesAndTheirIndices.entrySet()) {
+            currentEdge = edgeMapEntry.getKey();
+
+            if (currentEdge.firstVertice.equals(firstVertice) && currentEdge.secondVertice.equals(secondVertice)) {
+                return currentEdge;
             }
         }
 
