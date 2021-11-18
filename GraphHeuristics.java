9b7ba0ada19689c867466c66414abc15cd1a36fb
natan-alex <natancarmo321@gmail.com>
2021-11-18 20:07:49 -0300

    rewrite code in graph heuristics trying to make it better

--- GraphHeuristics.java
+++ GraphHeuristics.java
@@ -1,24 +1,21 @@
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GraphHeuristics {
     private final Graph relatedGraph;
-    private final String[] partsOfHeuristics;
+    private final String heuristicsStringRepresentation;
     public final Map<Vertice, Float> verticesAndTheirHeuristics;
-    private String[] verticeNameAndHeuristicValue;
 
     public GraphHeuristics(Graph graph, String heuristicsRepresentation) {
         relatedGraph = graph;
 
         verticesAndTheirHeuristics = new LinkedHashMap<>(relatedGraph.numberOfVertices);
+        heuristicsStringRepresentation = heuristicsRepresentation;
 
         checkIfHeuristicsRepresentationIsValid(heuristicsRepresentation);
 
-        partsOfHeuristics = heuristicsRepresentation.substring(1, heuristicsRepresentation.length() - 1).split(",");
-        
         fillHeuristicsForEachVertice();
     }
 
@@ -26,55 +23,55 @@ public class GraphHeuristics {
             "^\\s*\\[\\s*(?:\\s*\\w+\\s*:\\s*\\d+(?:\\.\\d+)?\\s*,)*\\s*\\w+\\s*:\\s*\\d+(?:\\.\\d+)?\\s*\\]\\s*",
             Pattern.MULTILINE);
 
-    private void checkIfHeuristicsRepresentationIsValid(String heuristicsRepresentation) throws IllegalArgumentException {
-        if (heuristicsRepresentation == null || heuristicsRepresentation.isEmpty())
-            return;
-
+    private void checkIfHeuristicsRepresentationIsValid(String heuristicsRepresentation)
+            throws IllegalArgumentException {
         Matcher matcher = PATTERN_TO_VALIDATE_THE_HEURISTICS.matcher(heuristicsRepresentation);
 
-        if (!matcher.matches()) 
+        if (!matcher.matches())
             throw new IllegalArgumentException("The heuristic " + heuristicsRepresentation
-            + " is not a valid heuristic. A valid heuristic is something like [ a: 1, b: 2.3, c: 3.0 ]" 
-            + " where the vertice is on the left of the : and the heuristic value is on the rigth.");
+                    + " is not a valid heuristic. A valid heuristic is something like [ a: 1, b: 2.3, c: 3.0 ]"
+                    + " where the vertice is on the left of the : and the heuristic value is on the rigth.");
+    }
+
+    private String[] splitHeuristicRepresentation() {
+        String heuristicsWithoutBracket = heuristicsStringRepresentation.substring(1,
+                heuristicsStringRepresentation.length() - 1);
+        return heuristicsWithoutBracket.split(",");
     }
 
     private void fillHeuristicsForEachVertice() throws IllegalArgumentException {
         int currentVerticeIndex = 0;
-        Optional<Vertice> verticeFound;
+        Vertice verticeFound;
+        String[] partsOfHeuristics = splitHeuristicRepresentation();
+        String[] verticeNameAndHeuristicValue;
 
-        for (Vertice vertice : relatedGraph.verticesAndTheirIndices.keySet()) {
-            currentVerticeIndex = relatedGraph.verticesAndTheirIndices.get(vertice);
-
+        for (Vertice vertice : relatedGraph.vertices) {
             if (currentVerticeIndex >= partsOfHeuristics.length) {
                 verticesAndTheirHeuristics.put(vertice, 0F);
             } else {
-                fillVerticeNameAndHeuristicValueAccordingToCurrentVerticeIndex(currentVerticeIndex);
+                verticeNameAndHeuristicValue = splitPartOfHeuristics(partsOfHeuristics[currentVerticeIndex]);
 
-                verticeFound = getTheVerticeWithThisName(verticeNameAndHeuristicValue[0]);
+                verticeFound = relatedGraph.getTheVerticeWithThisName(verticeNameAndHeuristicValue[0]);
 
-                checkIfOptionalContainingTheVerticeFoundIsEmpty(verticeFound);
+                throwsExceptionIfVerticeIsNull(verticeFound, verticeNameAndHeuristicValue[0]);
 
-                verticesAndTheirHeuristics.put(verticeFound.get(), Float.parseFloat(verticeNameAndHeuristicValue[1]));
+                verticesAndTheirHeuristics.put(verticeFound, Float.parseFloat(verticeNameAndHeuristicValue[1]));
             }
         }
     }
 
-    private void fillVerticeNameAndHeuristicValueAccordingToCurrentVerticeIndex(int currentVerticeIndex) {
-        verticeNameAndHeuristicValue = partsOfHeuristics[currentVerticeIndex].trim().split(":");
+    private String[] splitPartOfHeuristics(String partOfHeuristic) {
+        String[] verticeNameAndHeuristicValue = partOfHeuristic.trim().split(":");
         verticeNameAndHeuristicValue[0] = verticeNameAndHeuristicValue[0].trim();
         verticeNameAndHeuristicValue[1] = verticeNameAndHeuristicValue[1].trim();
+        return verticeNameAndHeuristicValue;
     }
 
-    private void checkIfOptionalContainingTheVerticeFoundIsEmpty(Optional<Vertice> verticeFound) {
-        if (verticeFound.isEmpty()) {
-            throw new IllegalArgumentException("The vertice " + verticeNameAndHeuristicValue[0]
-                    + " is not in the vertice set." + " The vertice set is: "
-                    + relatedGraph.verticesAndTheirIndices.keySet());
+    private void throwsExceptionIfVerticeIsNull(Vertice verticeFound, String verticeName)
+            throws IllegalArgumentException {
+        if (verticeFound == null) {
+            throw new IllegalArgumentException("The vertice " + verticeName + " is not in the vertice set."
+                    + " The vertice set is: " + relatedGraph.vertices);
         }
-    }
-
-    private Optional<Vertice> getTheVerticeWithThisName(String name) {
-        return relatedGraph.verticesAndTheirIndices.keySet().stream()
-                .filter(vertice -> vertice.name.compareTo(name) == 0).findFirst();
     }
 }
