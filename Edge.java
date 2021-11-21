07f2f53db3e83b881bf5ae4681dc995927b46709
natan-alex <natancarmo321@gmail.com>
2021-11-21 13:50:01 -0300

    remove spaces from edge representation and make edge value an int again

--- Edge.java
+++ Edge.java
@@ -2,23 +2,29 @@ public class Edge {
     public final String stringRepresentation;
     public final Vertice firstVertice;
     public final Vertice secondVertice;
-    public final float value;
+    public final int value;
 
     public Edge(String edgeRepresentation) throws IllegalArgumentException {
         EdgeValidator.checkIfEdgeRepresentationIsValid(edgeRepresentation);
 
-        stringRepresentation = edgeRepresentation.trim();
+        stringRepresentation = removeSpacesFromEdgeRepresentation(edgeRepresentation);
         String withoutParenthesisOrCurlyBraces = stringRepresentation.substring(1, stringRepresentation.length() - 1);
         String[] partsOfStringRepresentation = withoutParenthesisOrCurlyBraces.split(",");
 
-        firstVertice = new Vertice(partsOfStringRepresentation[0].trim());
-        secondVertice = new Vertice(partsOfStringRepresentation[1].trim());
+        firstVertice = new Vertice(partsOfStringRepresentation[0]);
+        secondVertice = new Vertice(partsOfStringRepresentation[1]);
 
         if (partsOfStringRepresentation.length == 3) {
-            value = Float.parseFloat(partsOfStringRepresentation[2].trim());
+            value = Integer.parseInt(partsOfStringRepresentation[2]);
         } else {
-            value = 0F;
+            value = 0;
         }
+    }
+
+    private String removeSpacesFromEdgeRepresentation(String edgeRepresentation) {
+        edgeRepresentation = edgeRepresentation.trim();
+        edgeRepresentation = edgeRepresentation.replaceAll("\\s*,\\s*", ",");
+        return edgeRepresentation;
     }
 
     @Override
