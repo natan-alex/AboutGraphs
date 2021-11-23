e6711f1706032525c428f3faa240645152b155d8
natan-alex <natancarmo321@gmail.com>
2021-11-23 18:55:43 -0300

    overriding to string and add current capacity in reversed direction

--- FlowEdge.java
+++ FlowEdge.java
@@ -1,11 +1,18 @@
 public class FlowEdge extends Edge {
     public int currentCapacity;
+    public int currentCapacityInReversedDirection;
     public int maximumCapacity;
 
     public FlowEdge(String edgeRepresentation, int maximumCapacity) throws IllegalArgumentException {
         super(edgeRepresentation);
         this.maximumCapacity = maximumCapacity;
         currentCapacity = 0;
+        currentCapacityInReversedDirection = 0;
     }
 
+    @Override
+    public String toString() {
+        return stringRepresentation + ": " + "+" + currentCapacity + ", -" + currentCapacityInReversedDirection + ", "
+                + maximumCapacity;
+    }
 }
