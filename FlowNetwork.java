48970a0528f8e1cded4bc99ae790f8d5d2b63262
natan-alex <natancarmo321@gmail.com>
2021-11-23 21:07:26 -0300

    reverse edge vertices for flow edges in reversed direction in flow network

--- FlowNetwork.java
+++ FlowNetwork.java
@@ -3,14 +3,29 @@ import java.util.List;
 
 public class FlowNetwork extends Graph {
     public final List<FlowEdge> flowEdges;
+    public final List<FlowEdge> flowEdgesInReversedDirection;
 
     public FlowNetwork(String graphRepresentation) throws IllegalArgumentException {
         super(graphRepresentation);
         flowEdges = new ArrayList<>(numberOfEdges);
+        flowEdgesInReversedDirection = new ArrayList<>(numberOfEdges);
 
         for (Edge edge : edges) {
             flowEdges.add(new FlowEdge(edge.stringRepresentation, edge.value));
+            flowEdgesInReversedDirection.add(new FlowEdge(createEdgeRepresentationWithVerticesReversed(edge), edge.value));
         }
+    }
+
+    private String createEdgeRepresentationWithVerticesReversed(Edge edge) {
+        StringBuffer newRepresentation = new StringBuffer();
+        newRepresentation.append("(");
+        newRepresentation.append(edge.secondVertice.name);
+        newRepresentation.append(",");
+        newRepresentation.append(edge.firstVertice.name);
+        newRepresentation.append(",");
+        newRepresentation.append(edge.value);
+        newRepresentation.append(")");
+        return newRepresentation.toString();
     }
 
     @Override
