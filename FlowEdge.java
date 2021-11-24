c099dd1b39a7f95195ac3feb4245e9e599b2c282
natan-alex <natancarmo321@gmail.com>
2021-11-23 21:06:54 -0300

    remove unnecessary and incorrect field

--- FlowEdge.java
+++ FlowEdge.java
@@ -1,18 +1,15 @@
 public class FlowEdge extends Edge {
     public int currentFlow;
-    public int currentFlowInReversedDirection;
     public int maximumCapacity;
 
     public FlowEdge(String edgeRepresentation, int maximumCapacity) throws IllegalArgumentException {
         super(edgeRepresentation);
         this.maximumCapacity = maximumCapacity;
         currentFlow = 0;
-        currentFlowInReversedDirection = 0;
     }
 
     @Override
     public String toString() {
-        return stringRepresentation + ": " + "+" + currentFlow + ", -" + currentFlowInReversedDirection + ", "
-                + maximumCapacity;
+        return stringRepresentation + ": " + currentFlow + ", " + maximumCapacity;
     }
 }
