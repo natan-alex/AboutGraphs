12b25baa54dde6ded0924398468d9a4a1008a030
natan-alex <natancarmo321@gmail.com>
2021-11-27 22:53:10 -0300

    rename how much flow field to currentFlowPassing

--- graph_core/FlowEdge.java
+++ graph_core/FlowEdge.java
@@ -1,17 +1,17 @@
 package aboutGraphs.core;
 
 public class FlowEdge extends Edge {
-    public int howMuchFlowCanStillPass;
+    public int currentFlowPassing;
     public int maximumCapacity;
 
     public FlowEdge(String edgeRepresentation, int maximumCapacity) throws IllegalArgumentException {
         super(edgeRepresentation);
         this.maximumCapacity = maximumCapacity;
-        howMuchFlowCanStillPass = 0;
+        currentFlowPassing = 0;
     }
 
     @Override
     public String toString() {
-        return stringRepresentation + ": " + howMuchFlowCanStillPass + ", " + maximumCapacity;
+        return stringRepresentation + ": " + currentFlowPassing + ", " + maximumCapacity;
     }
 }
