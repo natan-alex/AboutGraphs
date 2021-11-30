e0b5041ecca08c5c10017d5ee5168a2adf912e01
natan-alex <natancarmo321@gmail.com>
2021-11-30 09:21:38 -0300

    ignore intellij files

--- core/abstractions/AbstractEdge.java
+++ core/abstractions/AbstractEdge.java
@@ -1,7 +1,40 @@
+package AboutGraphs.core.abstractions;
+
 public abstract class AbstractEdge {
-    public abstract AbstractVertice getFirstVertice();
-    public abstract AbstractVertice getSecondVertice();
-    public abstract String getRepresentation();
+    private String representation;
+    private AbstractVertice firstVertice;
+    private AbstractVertice secondVertice;
+
+    protected abstract void validateEdgeRepresentation();
+
+    public AbstractEdge(String edgeRepresentation) {
+        representation = edgeRepresentation;
+        validateEdgeRepresentation();
+    }
+
+    public String getRepresentation() {
+        return representation;
+    }
+
+    protected void setRepresentation(String newRepresentation) {
+        representation = newRepresentation;
+    }
+
+    public AbstractVertice getFirstVertice() {
+        return firstVertice;
+    }
+
+    protected void setFirstVertice(AbstractVertice newVertice) {
+        firstVertice = newVertice;
+    }
+
+    public AbstractVertice getSecondVertice() {
+        return secondVertice;
+    }
+
+    protected void setSecondVertice(AbstractVertice newVertice) {
+        secondVertice = newVertice;
+    }
 
     @Override
     public boolean equals(Object obj) {
