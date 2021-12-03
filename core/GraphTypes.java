7858580974b3f6e5824372ffbd59b67f1411459e
natan-alex <natancarmo321@gmail.com>
2021-12-03 11:45:00 -0300

    add representation to graphtypes

--- core/GraphTypes.java
+++ core/GraphTypes.java
@@ -1,14 +1,30 @@
 package core;
 
 public enum GraphTypes {
-    DIRECTED_AND_PONDERED(true, true), DIRECTED_AND_UNPONDERED(true, false), UNDIRECTED_AND_PONDERED(false, true),
-    UNDIRECTED_AND_UNPONDERED(false, false);
+    DIRECTED_AND_PONDERED(true, true, "directed and pondered"),
+    DIRECTED_AND_UNPONDERED(true, false, "directed and unpondered"),
+    UNDIRECTED_AND_PONDERED(false, true, "undirected and pondered"),
+    UNDIRECTED_AND_UNPONDERED(false, false, "undirected and unpondered");
 
-    public boolean isPondered;
-    public boolean isDirected;
+    private final boolean isDirected;
+    private final boolean isPondered;
+    private final String representation;
 
-    private GraphTypes(boolean isDirected, boolean isPondered) {
+    private GraphTypes(boolean isDirected, boolean isPondered, String representation) {
         this.isDirected = isDirected;
         this.isPondered = isPondered;
+        this.representation = representation;
+    }
+
+    public boolean isPondered() {
+        return isPondered;
+    }
+
+    public boolean isDirected() {
+        return isDirected;
+    }
+
+    public String getRepresentation() {
+        return representation;
     }
 }
