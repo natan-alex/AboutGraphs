eab84107fa625c9cf07ef4208cb72c3ce3fd8b6c
natan-alex <natancarmo321@gmail.com>
2021-12-08 13:39:33 -0300

    make createVertice abstract in abstractEdge and get just vertice representation in abstractEdge

--- core/FlowEdge.java
+++ core/FlowEdge.java
@@ -1,6 +1,7 @@
 package core;
 
 import core.abstractions.AbstractFlowEdge;
+import core.abstractions.AbstractVertice;
 import core.validators.FlowEdgeValidator;
 
 public class FlowEdge extends AbstractFlowEdge {
@@ -9,12 +10,17 @@ public class FlowEdge extends AbstractFlowEdge {
     }
 
     @Override
-    public String toString() {
-        return getRepresentation() + ": " + getCurrentFlowPassing() + ", " + getMaximumCapacity();
+    protected void validateEdgeRepresentation() throws IllegalArgumentException {
+        FlowEdgeValidator.checkIfFlowEdgeRepresentationIsValid(getRepresentation());
     }
 
     @Override
-    protected void validateEdgeRepresentation() throws IllegalArgumentException {
-        FlowEdgeValidator.checkIfFlowEdgeRepresentationIsValid(getRepresentation());
+    protected AbstractVertice createVerticeFromRepresentation(String representation) {
+        return new Vertice(representation);
+    }
+
+    @Override
+    public String toString() {
+        return getRepresentation() + ": " + getCurrentFlowPassing() + ", " + getMaximumCapacity();
     }
 }
